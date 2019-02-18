730a069ac1e17854c3918ee3dcceb4d307ccffd2
Nam Ninja <namninja@gmail.com>
2019-02-18 15:08:20 -0800

    debugging heroku deployment

--- public/client.js
+++ public/client.js
@@ -15,7 +15,7 @@ var blogTemplate =
     "</div>" +
     "</div>";
 
-var serverBase = "mighty-fortress-33902.herokuapp.com//";
+var serverBase = "//mighty-fortress-33902.herokuapp.com/";
 //var serverBase = "//localhost:8080/";
 var BLOGS_URL = serverBase + "blog-posts";
 
